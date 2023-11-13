76ae6981675c5a050d529cdb2457754c1aedc4a2
Kartik-Ranaut <ranautk6@gmail.com>
2023-11-13 14:53:56 +0530

    deleted all files

--- README.md
+++
@@ -1,5 +0,0 @@
-# gitdemo
-hi my name is kartik
-this is demo
-trying to do pull request
-trying to do another pull request
--- js/home.js
+++
@@ -1,6 +0,0 @@
-// const ip1 = document.getElementById("ip1");
-// const ip2 = document.getElementById("ip2");
-// const btn = document.getElementById("btn");
-
-const ram= require("./form.js");
-ram.ram();
